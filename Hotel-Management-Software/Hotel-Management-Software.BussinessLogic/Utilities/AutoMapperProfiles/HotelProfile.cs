f84b25905194334e2dde30bac2645923be29d22d
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-14 19:44:08 +0200

    Hotel automapper profile

---
+++ Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Utilities/AutoMapperProfiles/HotelProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace Hotel_Management_Software.BBL.Utilities.AutoMapperProfiles;
+
+using AutoMapper;
+using Hotel_Management_Software.DAL.Entities;
+using Hotel_Management_Software.DTO.Hotel;
+
+public class HotelProfile : Profile
+{
+    public HotelProfile()
+    {
+        CreateMap<Hotel, HotelToAddDTO>().ReverseMap();
+    }
+}
