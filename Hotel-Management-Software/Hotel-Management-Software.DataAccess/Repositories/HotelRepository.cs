d2893b670ce6a3b1463442e156bf8804c47ae62b
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-14 14:39:25 +0200

    Added hotel repository.

---
+++ Hotel-Management-Software/Hotel-Management-Software.DataAccess/Repositories/HotelRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Hotel_Management_Software.DAL.Repositories;
+
+using aspnetcore.ntier.DAL.Repositories;
+using Hotel_Management_Software.DAL.Entities;
+using Hotel_Management_Software.DAL.Repositories.IRepositories;
+using Hotel_Management_Software.DataAccess.DataContext;
+
+public class HotelRepository : GenericRepository<Hotel>, IHotelRepository
+{
+    private readonly ContextDB _dbContext;
+
+    public HotelRepository(ContextDB context) : base(context)
+    {
+        _dbContext = context;
+    }
+}
---
+++ Hotel-Management-Software/Hotel-Management-Software.DataAccess/Repositories/IRepositories/IHotelRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Hotel_Management_Software.DAL.Repositories.IRepositories;
+
+using aspnetcore.ntier.DAL.Repositories.IRepositories;
+using Hotel_Management_Software.DAL.Entities;
+
+public interface IHotelRepository : IGenericRepository<Hotel>
+{
+}
