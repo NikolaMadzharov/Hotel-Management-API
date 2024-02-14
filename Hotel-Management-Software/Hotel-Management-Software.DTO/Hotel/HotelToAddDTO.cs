3d56d537fd7fbf05082e27b8dcfc25823e6fe784
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-14 19:40:40 +0200

    Created Add hotel DTO

---
+++ Hotel-Management-Software/Hotel-Management-Software.DTO/Hotel/HotelToAddDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Hotel_Management_Software.DTO.Hotel;
+
+
+public class HotelToAddDTO
+{
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string TelephoneNumber { get; set; } = null!;
+
+    public string MobilePhone { get; set; } = null!;
+
+    public byte[]? ProfilePicture { get; set; }
+
+    public string OwnerId { get; set; } = null!;
+}
