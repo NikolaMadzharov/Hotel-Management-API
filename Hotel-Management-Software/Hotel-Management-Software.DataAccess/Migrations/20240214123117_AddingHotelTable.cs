a4e730732e4af285fd31e9cc819f3547099e71ba
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-14 14:31:57 +0200

    Hotel table migration

---
+++ Hotel-Management-Software/Hotel-Management-Software.DataAccess/Migrations/20240214123117_AddingHotelTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Management_Software.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingHotelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    TelephoneNumber = table.Column<string>(type: "text", nullable: false),
+                    MobilePhone = table.Column<string>(type: "text", nullable: false),
+                    ProfilePicture = table.Column<byte[]>(type: "bytea", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
