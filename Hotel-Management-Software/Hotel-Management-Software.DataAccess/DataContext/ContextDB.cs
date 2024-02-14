3f484902b19286107f6eed939a4d66074947f8c2
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-14 14:23:59 +0200

    Added hotel dbset to context.

--- Hotel-Management-Software/Hotel-Management-Software.DataAccess/DataContext/ContextDB.cs
+++ Hotel-Management-Software/Hotel-Management-Software.DataAccess/DataContext/ContextDB.cs
@@ -1,24 +1,20 @@
-﻿
-namespace Hotel_Management_Software.DataAccess.DataContext;
+﻿namespace Hotel_Management_Software.DataAccess.DataContext;
+
+using Hotel_Management_Software.DAL.Entities;
 using Hotel_Management_Software.DAL.Entities.ApplicationUser;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
-
-
-public class ContextDB:IdentityDbContext<ApplicationUser>
+public class ContextDB : IdentityDbContext<ApplicationUser>
 {
 
     public ContextDB(DbContextOptions options) : base(options) { }
 
-  
+    public virtual DbSet<Hotel> Hotels { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-       
-
         base.OnModelCreating(builder);
-
     }
 }
