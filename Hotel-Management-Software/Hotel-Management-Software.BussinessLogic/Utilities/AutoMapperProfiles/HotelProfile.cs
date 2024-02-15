0d65f603a16a1e94f417c6f5dfc663eb188d2d35
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-15 23:47:42 +0200

    Added FileHelper

--- Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Utilities/AutoMapperProfiles/HotelProfile.cs
+++ Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Utilities/AutoMapperProfiles/HotelProfile.cs
@@ -1,30 +1,17 @@
 ﻿namespace Hotel_Management_Software.BBL.Utilities.AutoMapperProfiles;
 
 using AutoMapper;
+using Hotel_Management_Software.BBL.Helpers;
 using Hotel_Management_Software.DAL.Entities;
 using Hotel_Management_Software.DTO.Hotel;
-using Microsoft.AspNetCore.Http;
 
 public class HotelProfile : Profile
 {
     public HotelProfile()
     {
         CreateMap<Hotel, HotelToAddDTO>().ReverseMap()
-            .ForMember(dest => dest.ProfilePicture, opt => opt.MapFrom(src => ConvertIFormFileToByteArray(src.ProfilePicture)));
+            .ForMember(dest => dest.ProfilePicture, opt => opt.MapFrom(src => FileHelper.ConvertIFormFileToByteArray(src.ProfilePicture)));
 
         CreateMap<Hotel, HotelDTO>().ReverseMap();
-    }
-
-    private byte[] ConvertIFormFileToByteArray(IFormFile file)
-    {
-        if (file != null && file.Length > 0)
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                file.CopyTo(ms);
-                return ms.ToArray();
-            }
-        }
-        return null;
     }
 }
