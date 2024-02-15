132a1077ca290e4dcc81bcb07b53e0fbb4a22839
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-15 22:20:11 +0200

    Add room DTO and room DTO added

---
+++ Hotel-Management-Software/Hotel-Management-Software.DTO/Room/RoomToAddDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Hotel_Management_Software.DTO.Room;
+
+using System.ComponentModel.DataAnnotations;
+
+public class RoomToAddDTO
+{
+    [Required]
+    [Range(minimum: 1, maximum: 9999)]
+    public int RoomNumber { get; set; }
+
+    [Required]
+    public int Floor { get; set; }
+
+    [Required]
+    public Guid HotelId { get; set; }
+}
