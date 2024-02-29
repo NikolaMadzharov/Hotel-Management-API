6adf7d3201c0ba37df406780f568e5fe03685f04
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-29 23:34:30 +0200

    automapper profile for images

---
+++ Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Utilities/AutoMapperProfiles/ImageProfile.cs
@@ -0,0 +1,17 @@
+﻿namespace Hotel_Management_Software.BBL.Utilities.AutoMapperProfiles;
+
+using AutoMapper;
+using CloudinaryDotNet.Actions;
+using Hotel_Management_Software.DAL.Entities;
+using Hotel_Management_Software.DTO.Image;
+
+public class ImageProfile : Profile
+{
+    public ImageProfile()
+    {
+        CreateMap<RawUploadResult, FileUploadResult>();
+
+        CreateMap<FileUploadResult, Image>()
+            .ForMember(dest => dest.URL, opt => opt.MapFrom(src => src.SecureUrl));
+    }
+}
