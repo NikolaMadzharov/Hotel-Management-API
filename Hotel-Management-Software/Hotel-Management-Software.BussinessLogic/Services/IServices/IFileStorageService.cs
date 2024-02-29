f61006071d1586111ee4dea77da159ac3fa305ee
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-29 23:33:28 +0200

    Added file storage interfase

---
+++ Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Services/IServices/IFileStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_Management_Software.BBL.Services.IServices;
+
+using Hotel_Management_Software.DTO.Image;
+using Microsoft.AspNetCore.Http;
+
+public interface IFileStorageService
+{
+    Task<FileUploadResult> UploadAsync(IFormFile file, string path);
+}
