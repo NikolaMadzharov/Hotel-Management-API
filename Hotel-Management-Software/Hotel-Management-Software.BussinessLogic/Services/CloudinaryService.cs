7cf296c5c0c82263bba171cd0ef57d01c685539c
Ivan Kukutsov <ivankukucov@gmail.com>
2024-02-29 23:34:08 +0200

    added cloundry service.

---
+++ Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Services/CloudinaryService.cs
@@ -0,0 +1,41 @@
+﻿namespace Hotel_Management_Software.BBL.Services;
+
+using AutoMapper;
+using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Hotel_Management_Software.BBL.Services.IServices;
+using Hotel_Management_Software.DTO.Image;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+public class CloudinaryService : IFileStorageService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IMapper _mapper;
+    private readonly Cloudinary _cloudinary;
+
+    public CloudinaryService(IConfiguration configuration, IMapper mapper)
+    {
+        _configuration = configuration;
+        _mapper = mapper;
+        _cloudinary = new Cloudinary(_configuration["CLOUDINARY_URL"]);
+    }
+
+    public async Task<FileUploadResult> UploadAsync(IFormFile file, string path)
+    {
+        var uploadParams = new RawUploadParams
+        {
+            File = new FileDescription(file.FileName, file.OpenReadStream()),
+            PublicId = path,
+            UniqueFilename = true,
+            Overwrite = true,
+            Type = "authenticated"
+        };
+
+        var result = await _cloudinary.UploadAsync(uploadParams);
+
+        var uploadResult = _mapper.Map<FileUploadResult>(result);
+
+        return uploadResult!;
+    }
+}
---
+++ Hotel-Management-Software/Hotel-Management-Software.DTO/Image/FileUploadResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel_Management_Software.DTO.Image;
+
+using System.Net;
+
+public class FileUploadResult
+{
+    public HttpStatusCode? StatusCode { get; set; }
+
+    public string? PublicId { get; set; }
+
+    public string? SecureUrl { get; set; }
+}
