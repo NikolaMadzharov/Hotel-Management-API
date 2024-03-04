9b9f1948f2355998cfcd92b84016634bd911543b
Ivan Kukutsov <ivankukucov@gmail.com>
2024-03-04 22:51:14 +0200

    Ajusted namespace and usings.

--- Hotel-Management-Software/Hotel-Management-Software/Controllers/RoomController.cs
+++ Hotel-Management-Software/Hotel-Management-Software/Controllers/RoomController.cs
@@ -1,89 +1,88 @@
-﻿using Hotel_Management_Software.BBL.Services.IServices;
+﻿namespace Hotel_Management_Software.Controllers;
+
+using Hotel_Management_Software.BBL.Services.IServices;
 using Hotel_Management_Software.DTO.Room;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Hotel_Management_Software.Controllers
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class RoomController : Controller
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RoomController : Controller
+    private readonly IRoomService _roomService;
+
+    public RoomController(IRoomService roomService)
     {
-        private readonly IRoomService _roomService;
 
-        public RoomController(IRoomService roomService)
+        _roomService = roomService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateRoom([FromForm] RoomToAddDTO RoomToAddDTO)
+    {
+        var room = await _roomService.CreateAsync(RoomToAddDTO);
+
+
+        if (room is not null)
         {
-
-            _roomService = roomService;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateRoom([FromForm] RoomToAddDTO RoomToAddDTO)
-        {
-            var room = await _roomService.CreateAsync(RoomToAddDTO);
-
-
-            if (room is not null)
-            {
-                return Ok(new { room = room });
-
-            }
-            return BadRequest();
-
+            return Ok(new { room = room });
 
         }
+        return BadRequest();
 
-        [HttpGet("floor/{floorId}")]
-        public async Task<IActionResult> AllByFloorId(Guid floorId)
+
+    }
+
+    [HttpGet("floor/{floorId}")]
+    public async Task<IActionResult> AllByFloorId(Guid floorId)
+    {
+        var rooms = await _roomService.GetRoomsByFloorId(floorId);
+
+        if (rooms is not null)
         {
-            var rooms = await _roomService.GetRoomsByFloorId(floorId);
+            return Ok(new { rooms = rooms });
 
-            if (rooms is not null)
-            {
-                return Ok(new { rooms = rooms });
-
-            }
-
-
-            return BadRequest();
-        }
-        [HttpGet("{roomId}")]
-        public async Task<IActionResult> GetRoomById(Guid roomId)
-        {
-            var room = await _roomService.GetRoomByIdAsync(roomId);
-
-            if (room is not null)
-            {
-                return Ok(new { room = room });
-
-
-            }
-
-
-            return BadRequest();
         }
 
-        [HttpPost("AddExtra")]
-        public async Task<IActionResult> AddExtra(List<RoomExtraToAddDTO> roomExtraToAddDTO)
+
+        return BadRequest();
+    }
+    [HttpGet("{roomId}")]
+    public async Task<IActionResult> GetRoomById(Guid roomId)
+    {
+        var room = await _roomService.GetRoomByIdAsync(roomId);
+
+        if (room is not null)
         {
-            var roomExtra = await _roomService.AddRoomExtraAsync(roomExtraToAddDTO);
-
-            if (roomExtra is not null)
-            {
-                return Ok(new { roomExtra });
+            return Ok(new { room = room });
 
 
-            }
-            return BadRequest();
         }
 
-        [HttpDelete("extra/{id}")]
-        public async Task<IActionResult> DeleteExtra(Guid id)
-        {
-            await _roomService.RemoveRoomExtra(id);
 
-            return NoContent();
+        return BadRequest();
+    }
+
+    [HttpPost("AddExtra")]
+    public async Task<IActionResult> AddExtra(List<RoomExtraToAddDTO> roomExtraToAddDTO)
+    {
+        var roomExtra = await _roomService.AddRoomExtraAsync(roomExtraToAddDTO);
+
+        if (roomExtra is not null)
+        {
+            return Ok(new { roomExtra });
+
+
         }
+        return BadRequest();
+    }
+
+    [HttpDelete("extra/{id}")]
+    public async Task<IActionResult> DeleteExtra(Guid id)
+    {
+        await _roomService.RemoveRoomExtra(id);
+
+        return NoContent();
     }
 }
