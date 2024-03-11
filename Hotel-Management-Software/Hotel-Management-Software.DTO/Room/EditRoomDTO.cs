dcfc70cb2a163c890be3568cc29d6142c18e7137
Ivan Kukutsov <ivankukucov@gmail.com>
2024-03-11 22:43:29 +0200

    Added EditRoomDTO

---
+++ Hotel-Management-Software/Hotel-Management-Software.DTO/Room/EditRoomDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Hotel_Management_Software.DTO.Room;
+
+using System.ComponentModel.DataAnnotations;
+
+public class EditRoomDTO
+{
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    public int RoomNumber { get; set; }
+
+    [Required]
+    public Guid FloorId { get; set; }
+
+    [Required]
+    public bool IsCleaned { get; set; }
+}
