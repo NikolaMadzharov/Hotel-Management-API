02a1477d34ee4b416d5f509d765cfce04576ffe4
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-03-20 20:55:11 +0200

    Employee automaper profile added

---
+++ Hotel-Management-Software/Hotel-Management-Software.BussinessLogic/Utilities/AutoMapperProfiles/EmployeeProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Hotel_Management_Software.BBL.Utilities.AutoMapperProfiles;
+
+using AutoMapper;
+using Hotel_Management_Software.BussinessLogic.Helpers;
+using Hotel_Management_Software.DAL.Entities.ApplicationUser;
+using Hotel_Management_Software.DTO.Employee;
+
+public class EmployeeProfile : Profile
+{
+    public EmployeeProfile()
+    {
+        CreateMap<ApplicationUser, AddEmployeeDTO>().ReverseMap().ForMember(destination => destination.UserName, opt => opt.MapFrom(src => RandomGenerator.Generate().ToString()));
+
+        CreateMap<ApplicationUser, EmployeeDTO>().ReverseMap();
+    }
+}
