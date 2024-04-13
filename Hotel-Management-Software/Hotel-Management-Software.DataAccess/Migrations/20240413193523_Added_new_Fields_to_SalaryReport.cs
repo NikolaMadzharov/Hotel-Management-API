c0953fe88a87f69f2f455126abf715c00763aadc
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-04-13 22:36:53 +0300

    New salary report properties migration.

---
+++ Hotel-Management-Software/Hotel-Management-Software.DataAccess/Migrations/20240413193523_Added_new_Fields_to_SalaryReport.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Management_Software.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_new_Fields_to_SalaryReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Bonus",
+                table: "SalaryReports",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Penalty",
+                table: "SalaryReports",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bonus",
+                table: "SalaryReports");
+
+            migrationBuilder.DropColumn(
+                name: "Penalty",
+                table: "SalaryReports");
+        }
+    }
+}
