400ef79e68ada62818d737f86864ee0d9d59ad42
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-04-15 23:33:04 +0300

    Email change dtos added.

---
+++ Hotel-Management-Software/Hotel-Management-Software.DTO/User/EmailChangeRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Management_Software.DTO.User;
+
+using System.ComponentModel.DataAnnotations;
+
+public class EmailChangeRequestDTO
+{
+    [Required]
+    [EmailAddress]
+    public string NewEmail { get; set; } = null!;
+}
